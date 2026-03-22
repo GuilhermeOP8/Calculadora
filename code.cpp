ac665b02e8459e917ee4faa4d0a3cbf85bf568a2
Guilherme Patriarca <guilhermepatriarca8@gmail.com>
2026-03-22 23:37:32 +0000

    Correct variable declarations and input handling
    Fix syntax errors and improve input prompts.

--- code.cpp
+++ code.cpp
@@ -2,5 +2,16 @@
 using namespace std;
 
 int main () {
+  auto = number1
+  auto = number2
+  auto = operation
+
   cout << "Insira o primeiro número" << endl;
-  cin >> 
+  cin >> number 1
+  cout << "Insira o segundo número" << endl;
+  cin >> number2
+  cout << "Insira a operação" << endl;
+  cin >> operation
+  cout << number1, operation, number2
+    }
+
