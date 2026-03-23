033b5f9b66f198af3219cc55f9b6a962445ed09d
Guilherme Patriarca <guilhermepatriarca8@gmail.com>
2026-03-23 08:56:28 +0000

    Change prompt from 'Insira a operação' to 'Insira a conta'

--- code.cpp
+++ code.cpp
@@ -10,7 +10,7 @@ int main () {
   cin >> number1;
   cout << "Insira o segundo numero" << endl;
   cin >> number2;
-  cout << "Insira a operaçao" << endl;
+  cout << "Insira a conta" << endl;
   cin >> operation;
   if (operation == 1) {
     cout << number1+number2 << endl;
